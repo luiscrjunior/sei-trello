5247891ac0774acc94d41e130b830210b9ed0789
luis.reischak <luis.reischak@prf.gov.br>
2018-02-18 14:01:32 -0300

    removed unused function

--- src/js/view/utils/sei.js
+++ src/js/view/utils/sei.js
@@ -9,16 +9,3 @@ export const findAllProcessAnchors = () => {
   const anchors = document.querySelectorAll('a[class^="processo"]');
   return Array.prototype.slice.call(anchors);
 };
-
-export const findAnchorByProcessNumber = (processNumber) => {
-  const anchors = document.querySelectorAll('a[class^="processo"]');
-  let foundAnchor = null;
-  for (let i = 0; i < anchors.length; i++) {
-    const anchor = anchors[i];
-    if (anchor.textContent.trim() === processNumber) {
-      foundAnchor = anchor;
-      break;
-    }
-  }
-  return foundAnchor;
-};
