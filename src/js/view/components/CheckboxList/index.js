0c3fadadf5b51a2668098fd8eda51cf28d93275c
luis.reischak <luis.reischak@prf.gov.br>
2018-03-04 21:59:57 -0300

    added color-block and improved filter (still WIP)

--- src/js/view/components/CheckboxList/index.js
+++ src/js/view/components/CheckboxList/index.js
@@ -4,17 +4,19 @@ import classNames from 'classnames';
 
 class CheckboxList extends React.Component {
 
-  handleClick (key, e) {
-    console.log(key);
+  handleClick (isSelected, key, e) {
+    e.preventDefault();
+    if (!this.props.onClick) return;
+    this.props.onClick(!isSelected, key);
   }
 
   isSelected (key) {
-    if (!this.props.options.selection) {
+    if (!this.props.selected) {
       return false;
-    } else if (typeof this.props.options.selection === 'string') {
-      return (key === this.props.options.selection);
-    } else if (typeof this.props.options.selection === 'object') {
-      return (this.props.options.selection.indexOf(key) > -1);
+    } else if (typeof this.props.selected === 'string') {
+      return (key === this.props.selected);
+    } else if (typeof this.props.selected === 'object') {
+      return (this.props.selected.indexOf(key) > -1);
     } else {
       return false;
     }
@@ -22,14 +24,19 @@ class CheckboxList extends React.Component {
 
   renderItems () {
     if (!this.props.options) return null;
-    return this.props.options.items.map((item) => (
-      <li key={item.key} className={classNames(styles.item, { [styles.selected]: this.isSelected(item.key) })}>
-        <a href="#" className={styles.anchor} onClick={this.handleClick.bind(this, item.key)}>
-          <span className={styles.label}>{item.label}</span>
-          <i className={classNames('fas fa-check', styles['ic-check'])}></i>
-        </a>
-      </li>
-    ));
+    return this.props.options.map((item) => {
+      const isSelected = this.isSelected(item.key);
+      const colorStyle = item.key.replace('LABEL_', 'color-block-').toLowerCase();
+      return (
+        <li key={item.key} className={classNames(styles.item, { [styles.selected]: isSelected })}>
+          <a href="#" className={styles.anchor} onClick={this.handleClick.bind(this, isSelected, item.key)}>
+            <span className={classNames(styles['color-block'], styles[colorStyle], { hide: !this.props.color })}></span>
+            <span className={styles.label}>{item.label}</span>
+            <i className={classNames('fas fa-check', styles['ic-check'])}></i>
+          </a>
+        </li>
+      );
+    });
   }
 
   render () {
--- src/js/view/components/FilterPanel/index.js
+++ src/js/view/components/FilterPanel/index.js
@@ -4,61 +4,84 @@ import classNames from 'classnames';
 import FloatingPanel from 'view/components/FloatingPanel';
 import CheckboxList from 'view/components/CheckboxList';
 
-const options = {
-  process: {
-    multiple: false,
-    selection: 'NO_FILTER',
-    items: [
-      {
-        key: 'WITH_TRELLO_CARD',
-        label: 'Processos com cartão',
-      },
-      {
-        key: 'WITHOUT_TRELLO_CARD',
-        label: 'Processos sem cartão',
-      },
-    ],
-  },
-  labels: {
-    multiple: true,
-    selection: 'NO_FILTER',
-    items: [
-      {
-        key: 'NO_LABEL',
-        label: 'Sem Etiquetas',
-      },
-    ],
-  },
-  due: {
-    multiple: false,
-    selection: 'NO_FILTER',
-    items: [
-      {
-        key: 'WITH_INCOMPLETE_DUE',
-        label: 'Entrega em aberto',
-      },
-      {
-        key: 'WITH_COMPLETE_DUE',
-        label: 'Entrega concluída',
-      },
-      {
-        key: 'WITHOUT_DUE',
-        label: 'Sem data de entrega',
-      },
-    ],
-  },
+const defaultOptions = {
+  process: [
+    {
+      key: 'WITH_TRELLO_CARD',
+      label: 'Processos com cartão',
+    },
+    {
+      key: 'WITHOUT_TRELLO_CARD',
+      label: 'Processos sem cartão',
+    },
+  ],
+  labels: [
+    {
+      key: 'NO_LABEL',
+      label: 'Sem Etiquetas',
+    },
+  ],
+  due: [
+    {
+      key: 'WITH_INCOMPLETE_DUE',
+      label: 'Entrega em aberto',
+    },
+    {
+      key: 'WITH_COMPLETE_DUE',
+      label: 'Entrega concluída',
+    },
+    {
+      key: 'WITHOUT_DUE',
+      label: 'Sem data de entrega',
+    },
+  ],
 };
 
 class FilterPanel extends React.Component {
 
+  constructor (props) {
+    super(props);
+    this.state = { options: Object.assign({}, defaultOptions) };
+    this.state.options.labels = this.updateLabels(props.currentLabels);
+  }
+
+  updateLabels (currentLabels) {
+    return defaultOptions.labels.concat(currentLabels.map((newLabel) => {
+      return {
+        key: 'LABEL_' + newLabel.color.toUpperCase(),
+        label: newLabel.label,
+      };
+    }));
+  }
+
+  componentWillReceiveProps (nextProps) {
+    let currentOptions = Object.assign({}, this.state.options);
+    currentOptions.labels = this.updateLabels(nextProps.currentLabels);
+    this.setState({ options: currentOptions });
+  }
+
+  onItemClick (type, checked, key) {
+    console.log(type, checked, key);
+  }
+
   render () {
     return (
       <FloatingPanel title="Filtros">
-        <CheckboxList options={options.process} />
+        <CheckboxList
+          selected={this.props.filter.process}
+          options={this.state.options.process}
+          onClick={(checked, key) => this.onItemClick('process', checked, key)} />
         <hr />
-        <CheckboxList options={options.labels} />
+        <CheckboxList
+          color={true}
+          selected={this.props.filter.labels}
+          options={this.state.options.labels}
+          onClick={(checked, key) => this.onItemClick('labels', checked, key)} />
         <hr />
-        <CheckboxList options={options.due}/>
+        <CheckboxList
+          selected={this.props.filter.due}
+          options={this.state.options.due}
+          onClick={(checked, key) => this.onItemClick('due', checked, key)} />
       </FloatingPanel>
     );
   }
