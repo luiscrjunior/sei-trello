3622b24fa4d77e03519f5639ee58fabc59460ccb
luiscrjunior <luis.reischak@gmail.com>
2018-03-06 16:14:05 -0300

    added filter panel to filter button

--- src/js/view/components/TrelloFilterButton/index.js
+++ src/js/view/components/TrelloFilterButton/index.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import TrelloButton from 'view/components/TrelloButton';
+import FilterPanel from 'view/components/FilterPanel';
 import styles from './styles.scss';
 import classNames from 'classnames';
 
 class TrelloFilterButton extends React.Component {
 
+  constructor (props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
   onClick (e) {
     e.preventDefault();
-    if (!this.props.onClick) return;
-    if (this.props.isLoading) return;
-    this.props.onClick(e);
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  onPanelClose (e) {
+    this.setState({ isOpen: false });
   }
 
   render () {
     return (
-      <TrelloButton 
-        title="Filtrar"
-        icon="fas fa-filter" 
-        onClick={this.onClick.bind(this)}>
-
-      </TrelloButton>
+      <div className={styles.wrapper}>
+        <TrelloButton
+          title="Filtrar"
+          icon="fas fa-filter"
+          onClick={this.onClick.bind(this)}>
+        </TrelloButton>
+        <FilterPanel
+          className={classNames(styles['filter-panel'], { hide: !this.state.isOpen }) }
+          filter={{process: null, labels: null, due: null}}
+          onClose={this.onPanelClose.bind(this)}
+          onClick={this.onClick.bind(this)}> ></FilterPanel>  
+      </div>
     );
   }
 };
