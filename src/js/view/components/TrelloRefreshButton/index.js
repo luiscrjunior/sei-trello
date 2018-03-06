e042937c26bee08ece8cdd088f9716cd5b42cbf3
luiscrjunior <luis.reischak@gmail.com>
2018-03-06 08:24:43 -0300

    abstracted trello button and created trello refresh button

---
+++ src/js/view/components/TrelloRefreshButton/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import TrelloButton from 'view/components/TrelloButton';
+import loadingImg from './loading.svg';
+import styles from './styles.scss';
+import classNames from 'classnames';
+
+class TrelloRefreshButton extends React.Component {
+
+  onClick (e) {
+    e.preventDefault();
+    if (!this.props.onClick) return;
+    if (this.props.isLoading) return;
+    this.props.onClick(e);
+  }
+
+  render () {
+    return (
+      <TrelloButton 
+        title="Atualizar cartões do Trello"
+        icon="fas fa-sync-alt" 
+        onClick={this.onClick.bind(this)}>
+        <img src={loadingImg} className={classNames(styles.loadingImg, { hide: !this.props.isLoading })} />
+      </TrelloButton>
+    );
+  }
+};
+
+export default TrelloRefreshButton;
