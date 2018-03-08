253d88c935048e2f7c7590086fbbecea17aac588
luiscrjunior <luis.reischak@gmail.com>
2018-03-08 09:06:57 -0300

    close panel when clicked in the bg

--- src/js/view/components/FloatingPanel/index.js
+++ src/js/view/components/FloatingPanel/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import styles from './styles.scss';
 import classNames from 'classnames';
 
 class FloatingPanel extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.onBGClick = this.onBGClick.bind(this);
+  }
 
   onClose (e) {
     e.preventDefault();
@@ -10,9 +16,26 @@ class FloatingPanel extends React.Component {
     this.props.onClose();
   }
 
+  onBGClick (e) {
+    const clickedElement = e.target;
+    const panel = ReactDOM.findDOMNode(this.panel);
+    const elementInsideFilter = panel.contains(clickedElement);
+    if (!elementInsideFilter) this.onClose(e);
+  }
+
+  componentDidMount () {
+    document.querySelector('body').addEventListener('click', this.onBGClick);
+  }
+
+  componentWillUnmount () {
+    document.querySelector('body').removeEventListener('click', this.onBGClick);
+  }
+
   render () {
     return (
-      <div className={classNames(styles.panel, this.props.className)}>
+      <div
+        className={classNames(styles.panel, this.props.className)}
+        ref={(el) => { this.panel = el; }}>
         <span className={styles.title}>{this.props.title}</span>
         <a href="#" className={styles['btn-close']} onClick={this.onClose.bind(this)}>&times;</a>
         <div className={styles.content}>{this.props.children}</div>
