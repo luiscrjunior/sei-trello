34cf2347390579054fdeb500b973e8f7f8047ffb
luis.reischak <luis.reischak@prf.gov.br>
2018-04-22 10:35:42 -0300

    added google analytics to the bg page

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,9 @@
+import * as analytics from 'utils/analytics.js';
+
 chrome.runtime.onMessage.addListener((msg, sender) => {
   if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
     chrome.pageAction.show(sender.tab.id);
   }
 });
+
+analytics.load();
