5c25330ed0e79457f50745963b6cfc7d80910dee
luis.reischak <luis.reischak@gmail.com>
2018-06-08 10:54:05 -0300

    improved component layout

--- src/js/view/components/DuePanel/index.js
+++ src/js/view/components/DuePanel/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FloatingPanel from 'view/components/FloatingPanel';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 import classNames from 'classnames';
+
+import FloatingPanel from 'view/components/FloatingPanel';
+
+import 'react-datepicker/dist/react-datepicker.css';
+import styles from './styles.scss';
 
 class DuePanel extends React.Component {
 
-  constructor (props) {
-    super(props);
-  }
-
   render () {
     return (
-      <FloatingPanel>
-        Painel
+      <FloatingPanel title='Data de entrega'>
+        <div className={styles.wrapper}>
+          <DatePicker
+            inline
+            locale="pt-BR"
+          />
+        </div>
+        <div className={styles.wrapper}>
+          <button className='btn btn-primary'>Remover</button>
+        </div>
       </FloatingPanel>
     );
   }
