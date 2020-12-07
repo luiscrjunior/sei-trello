a62f1500455cd8c596fd0f62a3b59a62dbbb3e89
luis.reischak <luis.reischak@prf.gov.br>
2020-12-06 21:59:24 -0300

    added images file mock

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__tests__/fileMock.js',
+    '\\.(scss)$': 'identity-obj-proxy',
     'view/(.*)': '<rootDir>/src/js/view/$1',
-    '\\.(scss|svg)$': 'identity-obj-proxy',
+    'api/(.*)': '<rootDir>/src/js/api/$1',
+    'model/(.*)': '<rootDir>/src/js/model/$1',
   },
   testMatch: ['**/__tests__/**/*.test.js'],
 };
