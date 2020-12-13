45ceb83aaf9eac42073fcbb6ebc71676e339cb0e
luis.reischak <luis.reischak@prf.gov.br>
2020-12-13 11:07:27 -0300

    improved DuePanel playground

--- playground/components/DuePanel.js
+++ playground/components/DuePanel.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import DuePanel from 'view/components/DuePanel';
 import { TrelloCardBG } from '../styles';
 
 const DuePanelPlayground = () => {
+  const [due, setDue] = useState(null);
+  const [dueComplete, setDueComplete] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (!open) {
+      setOpen(true);
+    }
+  }, [open]);
+
   return (
     <TrelloCardBG>
-      <DuePanel due="2018-06-09T14:00:00.000Z" dueComplete={true} />
+      {open && (
+        <DuePanel
+          due={due}
+          dueComplete={dueComplete}
+          onChangeDue={(_due, _dueComplete) => {
+            setDue(_due);
+            setDueComplete(_dueComplete);
+            setOpen(false);
+          }}
+          onClose={() => setOpen(false)}
+        />
+      )}
     </TrelloCardBG>
   );
 };
