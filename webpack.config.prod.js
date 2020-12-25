8266816b1d4e5989c041ea783edb209e8294c59f
luis.reischak <luis.reischak@prf.gov.br>
2020-12-25 13:41:31 -0300

    removed duplicated directive

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,14 +1,8 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    new UglifyJSPlugin(),
-  ],
+  plugins: [new UglifyJSPlugin()],
 });
